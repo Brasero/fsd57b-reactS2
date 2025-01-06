3f564761797a294fa74923dbf6abddd92e97ea7b
Brasero <brandonricci4@gmail.com>
2025-01-06 11:54:42 +0100

    approfondissement formulaire

--- form/src/component/Form.jsx
+++ form/src/component/Form.jsx
@@ -30,9 +30,48 @@ const Form = () => {
   })
  }
  
+ const [textArea, setTextArea] = useState("Entrez votre message")
+ 
+ const handleTextChange = (e) => {
+  const {value} = e.target
+  setTextArea(value)
+ }
+ 
+ const [select, setSelect] = useState(["2", "4"])
+ 
+ const handleSelectChange = (e) => {
+  const {value} = e.target
+  let selectCopy = [...select]
+  if (select.includes(value)) {
+   selectCopy = selectCopy.filter((val) => val !== value)
+  } else {
+   selectCopy = selectCopy.concat([value])
+  }
+  setSelect(selectCopy)
+ }
+ 
+ const [check, setCheck] = useState(false)
+ 
+ const handleClick = () => {
+  setCheck(!check)
+ }
+ 
  return <>
   <form onSubmit={handleSubmit}>
    <input type="text" name={"name"} value={state.name} onChange={handleChange}/>
+   
+   <textarea value={textArea} onChange={handleTextChange} />
+   
+   <input type={"checkbox"} checked={check} onClick={handleClick}/>
+   
+   <select value={select} onChange={handleSelectChange} multiple={true}>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+   </select>
+   
    <input type={"submit"} value={"soumettre"}/>
   </form>
   {
